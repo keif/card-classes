b6e951745ed93b637ebb1a7b7f205c9e9d7e427d
Rev. Keith T. Baker <keith_baker@abercrombie.com>
2019-02-24 01:08:01 -0500

    made setDeck return a single array of cards, and shuffle via knuth method

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -5,19 +5,33 @@ const CARD_SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"];
 
 class Deck {
 	constructor() {
-		console.log("construct the deck of cards");
 		this.deck = [];
+		this.setDeck();
 	}
+
 	deal() {}
 
-	makeDeck() {
-		console.log("make a deck of cards");
-		this.deck = CARD_SUITS.map((suitVal, suitIdx, suitArr) =>
-			CARD_RANKS.map((rankVal, rankIdx, rankArr) => new Card(rankVal, suitVal))
-		);
+	setDeck() {
+		for (let suit of CARD_SUITS) {
+			for (let rank of CARD_RANKS) {
+				this.deck.push(new Card(rank, suit));
+			}
+		}
 	}
 
-	shuffle() {}
+	shuffle() {
+		let currentIndex = this.deck.length;
+
+		while (0 !== currentIndex) {
+			let randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// And swap it with the current element.
+			let temporaryValue = this.deck[currentIndex];
+			this.deck[currentIndex] = this.deck[randomIndex];
+			this.deck[randomIndex] = temporaryValue;
+		}
+	}
 }
 
 export default Deck;
