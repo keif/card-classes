3a5cf1b5f1ff1e984b06cdb3e04cd93d030d8e60
Rev. Keith T. Baker <keith_baker@abercrombie.com>
2019-02-24 01:00:53 -0500

    make the ranks the full word of non-numeric cards, and associate a symbol output

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
-const CARD_RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const CARD_RANKS = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 const CARD_SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"];
+const SUIT_SYMBOLS = ["♣︎", "♦", "♥︎", "♠︎"];
+
 
 class Card {
     constructor(rank, suit) {
-        console.log("new Card:", rank, suit);
         this.rank = rank;
         this.suit = suit;
+        this.symbol = SUIT_SYMBOLS[CARD_SUITS.indexOf(suit)];
         this.value = CARD_RANKS.indexOf(rank);
     }
 };
